8da7a5ec330f2513c31e6925e8d1a959f8d61522
Vadim Insapov <insapovvadik@gmail.com>
2021-05-31 01:43:15 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тренировка_строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string help = "3dfsggv";
+            int tt = (Convert.ToInt32((help[0]).ToString())) + 1;
+            Console.WriteLine(tt);
+            Console.ReadKey();
+        }
+    }
+}
